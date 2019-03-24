fe1b42b9f1954d6ea9469021f319b40a726577b9
aloneZERO <alonezero@foxmail.com>
2019-03-24 17:59:07 +0800

    [修改] StopWatch 1. 将 StopWatch 移动到工具包下; 2. 更换该工具类中使用的时间 API 为 JDK1.8 提供的新版时间 API。

--- src/main/java/utils/StopWatch.java
+++ src/main/java/utils/StopWatch.java
@@ -1,17 +1,20 @@
 package threesum;
 
+import java.time.Duration;
+import java.time.Instant;
+
 /**
  * 用于统计算法运行时间
  */
 public class StopWatch {
 
-    private static long start;
+    private static Instant start;
 
     /**
      * 开始计时
      */
     public static void start() {
-        start = System.currentTimeMillis();
+        start = Instant.now();
     }
 
     /**
@@ -20,7 +23,8 @@ public class StopWatch {
      * @return 已用时（单位：秒）
      */
     public static double elapsedTime() {
-        long now = System.currentTimeMillis();
-        return (now - start) / 1000.0;
+        Instant now = Instant.now();
+        return Duration.between(start, now)
+                .toMillis() / 1000.0;
     }
 }
--- src/test/java/threesum/test/RatioTest.java
+++ src/test/java/threesum/test/RatioTest.java
@@ -1,6 +1,6 @@
 package threesum.test;
 
-import threesum.StopWatch;
+import utils.StopWatch;
 import threesum.ThreeSum;
 import threesum.ThreeSumSlow;
 
@@ -11,19 +11,25 @@ import threesum.ThreeSumSlow;
  */
 public class RatioTest {
 
+    private static final ThreeSum THREE_SUM = new ThreeSumSlow();
+
     public static void main(String[] args) {
         int N = 500;
         int loopTimes = 7;
         double preTime = -1;
         while (loopTimes-- > 0) {
             int[] nums = new int[N];
+
             StopWatch.start();
-            ThreeSum threeSum = new ThreeSumSlow();
-            int cnt = threeSum.count(nums);
+
+            int cnt = THREE_SUM.count(nums);
             System.out.println(cnt);
+
             double elapsedTime = StopWatch.elapsedTime();
+
             double ratio = preTime == -1 ? 0 : elapsedTime / preTime;
             System.out.println(N + "  " + elapsedTime + "  " + ratio);
+
             preTime = elapsedTime;
             N *= 2;
         }
